a7139ea2c4884c74060b3bb20bff9f2b9c0fdeaf
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2022-06-24 03:22:16 +0200

    feat: code refactor

--- src/carousel.ts
+++ src/carousel.ts
@@ -3,14 +3,13 @@ import Component from './component';
 
 const visibleClass = 'is-hidden';
 
-const defaults = {
-  currentslide: 0,
-};
+const defaults = {};
 
 class Carousel extends Component {
   private readonly carousel: any;
   private readonly intro: Element | null;
   private readonly introBtn: Element | null | undefined;
+  private readonly navIntro: Element | null;
 
   public constructor(elem, params) {
     super(Carousel, elem, params);
@@ -18,15 +17,18 @@ class Carousel extends Component {
     this.carousel = this.elem?.querySelector('[data-dossier-carousel]');
     this.intro = this.elem?.querySelector('[data-dossier-intro]');
     this.introBtn = this.intro?.querySelector('.c-btn');
-    // data-dossier-navigation-item="intro
+    this.navIntro = this.elem?.querySelector('[data-dossier-navigation-intro]');
 
 
-    // intro
     this.introBtn?.addEventListener('click', (): void => {
         this.hideIntro();
         this.showCarousel();
     });
 
+    this.navIntro?.addEventListener('click', (): void => {
+      this.hideCarousel();
+      this.showIntro();
+    });
   }
 
   static get defaults() {
@@ -36,25 +38,19 @@ class Carousel extends Component {
   static create(elem, options) {
     return super.create(this, elem, options);
   }
-/*
-  private disableButton(button): void {
-    button?.setAttribute('disabled', '');
-  }
-*/
 
-  /*
   private showIntro(): void {
     this.intro?.classList.remove(visibleClass);
   }
-*/
+
   private hideIntro(): void {
     this.intro?.classList.add(visibleClass);
   }
-/*
+
   private hideCarousel(): void {
     this.carousel?.classList.add(visibleClass);
   }
-*/
+
   private showCarousel(): void {
     this.carousel?.classList.remove(visibleClass);
   }
